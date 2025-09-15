a0de94e8c0382364c88fd0c10dbef6628abfeca9
sethsteenken <sethsteenken@microsoft.com>
2025-09-15 12:39:12 -0400

    Infra - azure yml. remove scripts

--- infra/scripts/update-configurl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Updating APP Configuration Service URL in .env file..."
-# This script updates the deployed APP Configuration Service URL in .env file.
-sed -i "s|^APP_CONFIGURATION_URL=.*|APP_CONFIGURATION_URL=\"$(azd env get-value APP_CONFIGURATION_URL)\"|" ./src/.env
-
