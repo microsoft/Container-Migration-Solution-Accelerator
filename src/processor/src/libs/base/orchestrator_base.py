27261c7ecc81fdbc71e2a77f1a065e0c296ac197
Prachig-Microsoft <v-prachig@microsoft.com>
2026-06-12 17:50:25 +0530

    increase the tokens

--- src/processor/src/libs/base/orchestrator_base.py
+++ src/processor/src/libs/base/orchestrator_base.py
@@ -183,7 +183,7 @@ class OrchestratorBase(AgentBase, Generic[TaskParamT, ResultT]):
                     builder
                     .with_temperature(0.0)
                     .with_response_format(ManagerSelectionResponse)
-                    .with_max_tokens(4_000)
+                    .with_max_tokens(10_000)
                     .with_tools(agent_info.tools)  # for checking file existence
                 )
             elif agent_info.agent_name == "ResultGenerator":
